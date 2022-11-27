1667367e14042523a35302f54a360d8f1f1446f0
unknown <hienthai1234thptdh@gmail.com>
2022-11-27 22:14:19 +0700

    edit lengths

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -91,7 +91,7 @@ class SRDataLoader(LightningDataModule):
     def setup(self, stage=None):
         if stage == "fit":
             self.train, self.val = random_split(
-                SRDataset(data_dir=self.train_dir, img_size=self.img_size), lengths=[162080, 2600],
+                SRDataset(data_dir=self.train_dir, img_size=self.img_size), lengths=[160000, 2080],
                 generator=torch.Generator().manual_seed(0))
         elif stage == 'test':
             self.test = SRDataset(data_dir=self.test_dir,
