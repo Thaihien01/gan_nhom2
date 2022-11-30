62d1a400e031b0f9aab28b5360fa1dd86d2d5e0e
unknown <hienthai1234thptdh@gmail.com>
2022-11-30 16:58:51 +0700

    change lengths

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
     else:
         model = SRResNet()
     data = SRDataLoader(data_dir=args.data_dir, batch_size=args.batch_size)
-    data.prepare_data()
+    # data.prepare_data()
     # data.download_data()
     data.split_data()
     # data.setup('fit')
