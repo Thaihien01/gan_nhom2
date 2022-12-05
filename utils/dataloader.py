e940232ae87ec528c1ecf3e3df38e98ea9bb0435
unknown <hienthai1234thptdh@gmail.com>
2022-12-05 11:15:09 +0700

    edit train

--- utils/dataloader.py
+++ utils/dataloader.py
@@ -50,7 +50,7 @@ class SRDataset(Dataset):
         interpolated_img = self.setSize(lr_img)
         lr_img, hr_img, interpolated_img = self.toTensor(
             lr_img), self.toTensor(hr_img), self.toTensor(interpolated_img)
-        return lr_img, hr_img, interpolated_img
+        return lr_img.cuda(), hr_img.cuda(), interpolated_img.cuda()
 
 
 class SRDataLoader(LightningDataModule):
